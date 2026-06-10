5882c6b9137864d5e3c7f1bbbb77034dd31819d6
Elisandro Sams <elisandrosams@gmail.com>
2026-06-10 15:16:01 -0600

    feat: added loading state

--- books.js
+++ books.js
@@ -1,8 +1,11 @@
-function renderBooks(filter) {
+let books;
+
+async function renderBooks(filter) {
 
     const booksWrapper = document.querySelector('.books');
-
-    const books = getBooks();
+if (!books) {
+    books = await getBooks();
+}
 
 if (filter === 'LOW_TO_HIGH') {
     books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
@@ -62,8 +65,10 @@ setTimeout(() => {
 
 // FAKE DATA
 function getBooks() {
-  return [
-    {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([
+                {
       id: 1,
       title: "Crack the Coding Interview",
       url: "assets/crack the coding interview.png",
@@ -151,5 +156,7 @@ function getBooks() {
       salePrice: null,
       rating: 4.5,
     },
-  ];
+    ]);
+    }, 1000);
+    });
 }
